9cee161bf6135d22d96c2b8ddb2250bf1a54e0c0
Mayank Singh Rawat <mayankdai101997@gmail.com>
2025-09-27 10:52:12 +0530

    Update The Cow-Signal.cpp

--- The Cow-Signal.cpp
+++ The Cow-Signal.cpp
@@ -6,6 +6,8 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
+    freopen("cowsignal.in","r",stdin);
+    freopen("cowsignal.out","w",stdout);
     int row, col, k;
     cin >> row >> col >> k;
 
@@ -21,15 +23,16 @@ int main(){
     for (int i = 0; i< row; i++){
         string result;
         for (int j = 0; j< col; j++){
-            result += before[i][j];
-            result += before[i][j];
+          
+            for (int j1 = 0; j1 < k; j1++)
+                result += before[i][j];
         }
       
         for (int j = 0;j < k; j++){
             after.push_back(result);
            
         }
-    }   
+    }
    
     for (auto &it : after){
         cout << it << "\n";
